d6dd7781403380a965c799e8629d014c3dc65f93
jesus12279 <chucho12279@gmail.com>
2021-12-07 22:43:09 -0600

    Changing topic once again

--- user_instance.py
+++ user_instance.py
@@ -1,14 +1,16 @@
-from users import get_formatted_name
+"""Importing from our previously built file users.py"""
+from users import User
 
-print("Enter 'q' at any time to quit.")
-
+print("Enter 'q' at any time to quit. ")
+"""Asking for user input and giving them a way to end the program when needed"""
 while True:
-	first = input("\nPlease give me a first name: ")
+	first = input("\n Please give me a first name")
 	if first == 'q':
 		break
-	last = input("\nPlease give me alast name: ")
+	last = input("Please give me a last name")
 	if last == 'q':
 		break
-	formatted_name = get_formatted_name(first, last)
-	print("\t Neatly formatted name: " + formatted_name + "!")
+	"""Calling upon the User function and passing the user input"""
+	formatted_name = User(first, last)
+	print("\nNeatly formatted name: " + formatted_name.greet_user() + "!")
 
--- users.py
+++ users.py
@@ -1,3 +1,13 @@
-def get_formatted_name(first, last):
-    full_name = first + ' ' + last
-    return full_name.title()
+"""I am starting over on this file because i encountered some errors trying to get my previouse class working"""
+
+class User():
+    """Starting this class to pass user input and return a neatly formatted string,
+       after getting this class to work i will run some python test to make sure my code does not break"""
+	def __init__(self, first, last):
+	"""Initiating the class with the __init__ functoin and routing our values to appropaite the first and last name"""
+		self.first = first
+		self.last = last
+
+	def greet_user(self):
+	"""Simple function to return neatly formatted name when called upon"""
+		return self.first + " " + self.last
